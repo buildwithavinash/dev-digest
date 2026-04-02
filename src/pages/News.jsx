4ea53d68d9f161bf9da830623d858be365f6b930
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-02 21:21:32 +0530

    feat: filter by category

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -8,6 +8,10 @@ const News = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [category, setCategory] = useState("All")
+  const [visible, setVisible] = useState(6);
+
+  const categories = ["All", "AI", "Web Dev", "Startups"]
 
   useEffect(()=> {
     const getNews = async () => {
@@ -26,13 +30,28 @@ const News = () => {
     getNews();
   }, [])
 
+  const filterArticles = category === "All" ? articles : articles.filter((article)=> article.title.toLowerCase().includes(category.toLowerCase()));
+
   console.log(articles);
   return (
     <div className="pb-20">
       <h1 className="text-center text-3xl font-medium text-slate-900">Tech News</h1>
 
+
+{/* tabs */}
+
+<div className="flex gap-2 justify-center my-8">
+    {categories.map((cat)=>(
+      <button className="border border-slate-400 bg-slate-200 px-2 py-0.5 rounded-md cursor-pointer hover:bg-slate-300 transition-all" onClick={()=> setCategory(cat)}>
+        {cat}
+      </button>
+    ))}
+</div>
+
+
+
       {loading && (
-        <div>
+        <div className="mt-4">
           {Array.from({length:6}).map((_, i) => (
             <NewsSkeleton key={i} />
           ))}
@@ -44,10 +63,25 @@ const News = () => {
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
-        {articles.slice(0, 10).map((article, index) => (
+        {filterArticles.length === 0 && (
+          <div className="text-center">
+            <p className="text-slate-500">No articles found.</p>
+          </div>
+        )}
+        {filterArticles.slice(0, visible).map((article, index) => (
           <NewsCard key={index} article={article}/>
         ))}
       </div>
+
+
+      {/* load more button */}
+      {visible < filterArticles.length && (
+        <div className="flex justify-center mt-4">
+          <button onClick={()=>setVisible((prev) => prev + 6)} className="bg-slate-200 px-4 py-2 rounded-md cursor-pointer">
+            Load More
+          </button>
+        </div>
+      )}
     </div>
     
   )
